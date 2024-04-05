520d62003b3876a91ea142687d5127b6860f0554
root <root@kali.desec>
2024-04-05 06:48:59 -0500

    banner graping

--- banner_graping/banner_graping.py
+++ banner_graping/banner_graping.py
@@ -1,19 +1,25 @@
 import socket
+import sys
 
-def banner_grabing(port,host):
-	try:
-		s = socket.socket()
-		s.timeout(2)
-		s.connect((host,port))
-		banner=s.recv(1024)
-		return banner
-		
-	except Exception as e:
+def banner_grabbing(port, host):
+    try:
+        s = socket.socket()
+        s.settimeout(2)
+        s.connect((host, port))
+        banner = s.recv(1024)
+        return banner
+    except Exception as e:
+        # Note que a indentação abaixo usa 4 espaços
         return f"Não foi possível conectar ou receber o banner: {e}"
     finally:
         s.close()
-        
-#host = 'exemplo.com'  
-#port = 80  
-#banner = banner_grabbing(host, port)
-#print(banner)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Uso: python banner_graping.py <host> <port>")
+        sys.exit(1)
+    host = sys.argv[1]
+    port = int(sys.argv[2])
+    banner = banner_grabbing(port, host)
+    print(banner)
+
